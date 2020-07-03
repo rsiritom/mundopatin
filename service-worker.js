9d53d7597a2a738b60d8299e1aeebf3781927c68
rsiritom <ricardo.a.siri@gmail.com>
2020-07-02 23:48:43 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/https//rsiritom.github.io/mundopatin/precache-manifest.722d33c88af04c2abc1667eb0ee55786.js"
+  "/https//rsiritom.github.io/mundopatin2/precache-manifest.a2f0dac6d0fce2400dad5d075410c918.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https//rsiritom.github.io/mundopatin/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https//rsiritom.github.io/mundopatin2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
