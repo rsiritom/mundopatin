5314e14de96f85de670b2bb3caa0eca04238389e
rsiritom <ricardo.a.siri@gmail.com>
2020-07-02 23:55:46 -0300

    Updates

---
+++ precache-manifest.d6590110d0a8f789e144e01832d0b7bb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49ec58b7bc2b2851ba1447b151ca25cc",
+    "url": "/mundopatin2/index.html"
+  },
+  {
+    "revision": "e56ea09c102abd84edb5",
+    "url": "/mundopatin2/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "7ea9e897041b49508f20",
+    "url": "/mundopatin2/static/css/main.dcfc1d13.chunk.css"
+  },
+  {
+    "revision": "e56ea09c102abd84edb5",
+    "url": "/mundopatin2/static/js/2.2b3e7ab6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mundopatin2/static/js/2.2b3e7ab6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ea9e897041b49508f20",
+    "url": "/mundopatin2/static/js/main.8525892d.chunk.js"
+  },
+  {
+    "revision": "577a36dbba75d6057378",
+    "url": "/mundopatin2/static/js/runtime-main.6533811b.js"
+  },
+  {
+    "revision": "388620bb8f1c63d5980dfcbc75fbddc2",
+    "url": "/mundopatin2/static/media/WS2020.388620bb.jpg"
+  },
+  {
+    "revision": "2495b422e1641bc6679c1a424547d39d",
+    "url": "/mundopatin2/static/media/defaultBcg.2495b422.jpg"
+  },
+  {
+    "revision": "63c6969d67529b879b565d8176498a07",
+    "url": "/mundopatin2/static/media/galeria-1.63c6969d.jpg"
+  },
+  {
+    "revision": "653e865f659b73047abb5760b7d61e40",
+    "url": "/mundopatin2/static/media/galeria-2.653e865f.jpg"
+  },
+  {
+    "revision": "189e6536b8e004a83b15289be13c57a4",
+    "url": "/mundopatin2/static/media/galeria-3.189e6536.jpg"
+  },
+  {
+    "revision": "f03b8eb56a4eeeb4f0635368b37795b7",
+    "url": "/mundopatin2/static/media/galeria-4.f03b8eb5.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/https//rsiritom.github.io/mundopatin2/precache-manifest.a2f0dac6d0fce2400dad5d075410c918.js"
+  "/mundopatin2/precache-manifest.d6590110d0a8f789e144e01832d0b7bb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https//rsiritom.github.io/mundopatin2/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/mundopatin2/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
