151bda7b83d2556a548b079b14966541a32f1dca
rsiritom <ricardo.a.siri@gmail.com>
2020-07-02 23:24:38 -0300

    Updates

---
+++ precache-manifest.ae63301be043e93a0cf1aef525310b14.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9584deb472349170185f95362d898dfd",
+    "url": "/http//rsiritom.github.io/mundopatin/index.html"
+  },
+  {
+    "revision": "6d8b0778a2d15ac70305",
+    "url": "/http//rsiritom.github.io/mundopatin/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "cbf01d644de6b0747928",
+    "url": "/http//rsiritom.github.io/mundopatin/static/css/main.256a6328.chunk.css"
+  },
+  {
+    "revision": "6d8b0778a2d15ac70305",
+    "url": "/http//rsiritom.github.io/mundopatin/static/js/2.d52162b5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/http//rsiritom.github.io/mundopatin/static/js/2.d52162b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbf01d644de6b0747928",
+    "url": "/http//rsiritom.github.io/mundopatin/static/js/main.d68d080d.chunk.js"
+  },
+  {
+    "revision": "fbb77ead819a492bac5f",
+    "url": "/http//rsiritom.github.io/mundopatin/static/js/runtime-main.807e5003.js"
+  },
+  {
+    "revision": "388620bb8f1c63d5980dfcbc75fbddc2",
+    "url": "/http//rsiritom.github.io/mundopatin/static/media/WS2020.388620bb.jpg"
+  },
+  {
+    "revision": "2495b422e1641bc6679c1a424547d39d",
+    "url": "/http//rsiritom.github.io/mundopatin/static/media/defaultBcg.2495b422.jpg"
+  },
+  {
+    "revision": "63c6969d67529b879b565d8176498a07",
+    "url": "/http//rsiritom.github.io/mundopatin/static/media/galeria-1.63c6969d.jpg"
+  },
+  {
+    "revision": "653e865f659b73047abb5760b7d61e40",
+    "url": "/http//rsiritom.github.io/mundopatin/static/media/galeria-2.653e865f.jpg"
+  },
+  {
+    "revision": "189e6536b8e004a83b15289be13c57a4",
+    "url": "/http//rsiritom.github.io/mundopatin/static/media/galeria-3.189e6536.jpg"
+  },
+  {
+    "revision": "f03b8eb56a4eeeb4f0635368b37795b7",
+    "url": "/http//rsiritom.github.io/mundopatin/static/media/galeria-4.f03b8eb5.jpeg"
+  }
+]);
